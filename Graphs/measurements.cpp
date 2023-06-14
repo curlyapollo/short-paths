6d4a41ae4f02f65bbd3a6a2f8aedd28ab499dc4a
Leonid Karlinskiy <95444064+curlyapollo@users.noreply.github.com>
2023-06-14 14:30:47 +0300

    Update measurements.cpp

--- Graphs/measurements.cpp
+++ Graphs/measurements.cpp
@@ -16,7 +16,7 @@ double measurement_ford_bellman(int n, std::vector<std::vector<int64_t>> &graph)
     long long time = 0;
     for (int i = 0; i < 5; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
-        ford_bellman(graph, n, n - 1);
+        floyd_warshall(graph, n, n - 1);
         time += std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start).count();
     }
     return time / 5.0;
